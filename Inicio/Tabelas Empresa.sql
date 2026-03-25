a1c421f003156eb6d54d5cdd6ece5ec30b5e14c4
Cassiano Borba <bcassiano888@gmail.com>
2026-03-24 21:43:12 -0300

    Update Tabelas Empresa.sql

--- Inicio/Tabelas Empresa.sql
+++ Inicio/Tabelas Empresa.sql
@@ -1,1 +1,5 @@
 --É preciso obter mais dados da empresa, código está incompleto
+
+Select e.nome || 'trabalha para ' || g.nome "Quem trabalha para quem?" 
+From tb_empregados e
+Inner Join tb_empregados g on (e.rg_gerente = g.rg); -- se true, faz a a relação dos empregados que trabalham para os gerentes
